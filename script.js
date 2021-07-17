25c8fc4765687853fd5f2f4bb29c59f264290888
angadtewari09 <despacito0099@gmail.com>
2021-07-17 11:18:40 +0530

    Javascript functions added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var temp, maxtemp, mintemp, desc, wind, hum, main, day, night, target=0;
+var temp, maxtemp, mintemp, desc, wind, hum, main, day, night, target=0, feels_like;
 var x = [
     "clear sky",
     "few clouds",
@@ -47,13 +47,15 @@ var z = [
 
 function weather(input) {
     target = 1;
+    if(input === "") {
+        alert("Enter a City!!");
+    }
     var city = input;
-    //alert(city);
+    
     var img = document.querySelector('#icon'); //.src='icons/night cloudy.png';
     var myurl = "https://api.openweathermap.org/data/2.5/weather?";
     var key = "ae121a1d8e877330b5324ff5e2c4067e";
     var url = myurl +"APPID=" + key +"&units=metric&q=" + city; 
-    //alert(url);
 
     fetch(url)
     .then(function (response){
@@ -63,18 +65,16 @@ function weather(input) {
     .then(function(data) {
         console.log(data);
         
-        //alert(temperature);
-        //feels like section to be added
-        //document.getElementById('feels_like').innerHTML = Math.round(data.main.feels_like);
         temp = data.main.temp;
         mintemp = data.main.temp_min;
         maxtemp = data.main.temp_max;
+        feels_like = data.main.feels_like;
         document.getElementById('deg').innerHTML = Math.round(data.main.temp) + "&deg; C";
         document.querySelector('#min').innerHTML = Math.round(data.main.temp_min) + "&deg; C";
         document.querySelector('#max').innerHTML = Math.round(data.main.temp_max) + "&deg; C";
         document.getElementById('wind').innerHTML = Math.round(data.wind.speed) + "mph";
         document.getElementById('humidity').innerHTML = Math.round(data.main.humidity) + "%";
-        document.querySelector("#feels_like").innerHTML = Math.round(data.main.feels_like) + "&deg; C";
+        document.querySelector("#feels_like").innerHTML = Math.round(feels_like) + "&deg; C";
 
         desc = data.weather[0].description;
         document.getElementById('description').innerHTML = desc;
@@ -82,7 +82,6 @@ function weather(input) {
         day = data.sys.sunrise;
         night = data.sys.sunset;
         if( den>=night){
-            //alert("night");
             switch(desc){
                 case x[0]:
                     img.src = z[0];
@@ -127,7 +126,6 @@ function weather(input) {
 
 
         }else if(den >=day) {
-            //alert("day");
             switch(desc){
                 case x[0]:
                     img.src = y[0];
@@ -172,26 +170,29 @@ function weather(input) {
         }
 
     })
-    .catch(error => alert(error));
+    .catch(error => console.log(error));
 }
 //End of function 
 
 //function to convert into fahrenheit
 function converter(temperature) {
-    if(target != 1) {
+    if(target === 1) {
         alert("ENTER SOME CITY!!");
     }
     else if(temperature == 'fahrenheit') {
         var far = (temp * 9/5) +32;
         var min_far = (mintemp * 9/5) +32;
         var max_far = (maxtemp * 9/5) +32;
+        var feel_far = (feels_like * 9/5) +32;
 
         document.getElementById('deg').innerHTML = Math.round(far) + "&deg; F";
         document.querySelector('#min').innerHTML = Math.round(min_far) + "&deg; F";
         document.querySelector('#max').innerHTML = Math.round(max_far) + "&deg; F";
+        document.querySelector("#feels_like").innerHTML = Math.round(feel_far) + "&deg; F";
     }else {
         document.getElementById('deg').innerHTML = Math.round(temp) + "&deg; C";
         document.querySelector('#min').innerHTML = Math.round(mintemp) + "&deg; C";
         document.querySelector('#max').innerHTML = Math.round(maxtemp) + "&deg; C";
+        document.querySelector("#feels_like").innerHTML = Math.round(feels_like) + "&deg; C";
     }
 }
